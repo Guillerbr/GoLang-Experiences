ec0d6f771f6a1e47dcf1e98ce7a6871a38c4a131
Guillerbr <guillerbrasilrj@gmail.com>
2020-12-13 22:53:07 -0300

    second commit

--- main.go
+++ main.go
@@ -12,9 +12,21 @@ func main() {
 
 	var c string
 
+	d := euuu;
+	e := eooo;
+	f :=  iuuu;
+	
 	c = "Guiller";
 	
 	fmt.Println(a+b);
 	fmt.Println(c);
+
+	fmt.Println(c);
+
+	fmt.Println("%v \n", d);
+	fmt.Println("%v \n", e);
+	fmt.Println("%v \n", f);
+
+
 	
 }
