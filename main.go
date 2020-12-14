c6cac63c6784027e927b6ff53f8839c970bc707f
Guillerbr <guillerbrasilrj@gmail.com>
2020-12-13 22:45:53 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,20 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+
+import "fmt"
+
+
+func main() {
+	var a = 1
+	var b = 2
+
+	var c string
+
+	c = "Guiller";
+	
+	fmt.Println(a+b);
+	fmt.Println(c);
+	
+}
